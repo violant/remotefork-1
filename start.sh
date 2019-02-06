aaa7058608787e2e01d72d19afdb9f98a37035a3
Tarmo <dadacable@gmail.com>
2019-02-06 17:20:59 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 /opt/acestream.engine/start_acestream_chroot.sh >/dev/null 2>&1 &
 
-cd /app/
+cd /opt/linux-x64/
 chmod +x RemoteForkCP
 ./RemoteForkCP $1 >/dev/null 2>&1 &
 
 while true; do
-        sleep $2
+        sleep 600
         rm -rf /opt/acestream.engine/androidfs/acestream.engine/.ACEStream/collected_torrent_files/*
 done
