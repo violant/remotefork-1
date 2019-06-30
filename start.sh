6eb19ca882957941cde33763e142325c782cdcfb
tarmets <dadacable@gmail.com>
2019-06-30 20:16:15 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-/opt/acestream.engine/start_acestream_chroot.sh >/dev/null 2>&1 &
+/opt/acestream.engine/start-engine --client-console --cache-dir /tmp/.ACEStream --state-dir /tmp/.ACEStream --live-cache-type memory --vod-cache-type memory --live-buffer 3 --vod-buffer 5 --vod-drop-max-age 120 >/dev/null 2>&1 &
 
 cd /opt/linux-x64/
 chmod +x RemoteForkCP
 ./RemoteForkCP $1 >/dev/null 2>&1 &
 
 while true; do
-        sleep 600
-        rm -rf /opt/acestream.engine/androidfs/acestream.engine/.ACEStream/collected_torrent_files/*
+        sleep 1200
+        rm -rf /tmp/.ACEStream/collected_torrent_files/*
 done
